62b5885ad4fbb06c54db426d2a5674f89905aae0
orchid <orchid@241.com>
2026-03-25 15:26:30 +0900

    user02 modify

--- test03.py
+++ test03.py
@@ -3,9 +3,7 @@ def sayHello(name):
     return 'Hello, ' + name + '!'
     
 inName = 'Tom'
-print(sayHello(inName))
-<<<<<<< HEAD
+lastName = 'Brown'
+print(sayHello(inName + ' ' + lastName)))
 print("work02에서 커밋 생성")
-=======
 print("main 브랜치에서 새로운 커밋 생성")
->>>>>>> d77b825ed7e7792f1a0dd2cc89cd55871eea0787
